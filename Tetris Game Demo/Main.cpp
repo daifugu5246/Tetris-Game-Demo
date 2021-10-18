1951e1c62215f8f484628ba69ab13fdd9d0c5f18
PATIPAT KOEDDANG <64010475@KMITL.AC.TH>
2021-10-18 12:55:24 +0700

    Update

--- Tetris Game Demo/Main.cpp
+++ Tetris Game Demo/Main.cpp
@@ -1,46 +1,59 @@
 #include <SFML/Graphics.hpp>
+#include<iostream>
+#include <vector>
 #include <time.h>
 #include <windows.h>
-float Colcheck = 0;
+
+using namespace std;
+
+
+struct Tetris
+{
+	sf::Sprite block[2][4];
+	sf::Vector2f position;
+	bool state;
+
+};
+vector<Tetris> blocks;
 int main()
 {
-    srand(time(0));
-    sf::Clock clock;
-    sf::RenderWindow window; 
-    sf::Texture blockTexture;
-    blockTexture.loadFromFile("Sprite/blocks.png");
-    sf::Vector2u blocksize = blockTexture.getSize();
-    window.create(sf::VideoMode((blocksize.x/8)*10, (blocksize.x / 8)*20), "Tetris Game",sf::Style::Default);
-    sf::Sprite block[8];
-    block[0].setTexture(blockTexture);
-    block[0].setTextureRect(sf::IntRect(0, 0, blocksize.x/8, blocksize.y/5));
+	srand(time(NULL));
+	sf::Clock clock;
+	float deltaTime;
+	sf::RenderWindow window;
+	sf::Texture blockTexture;
+	blockTexture.loadFromFile("Sprite/blocks.png");
+	sf::Vector2u blocksize = blockTexture.getSize();
+	window.create(sf::VideoMode((blocksize.x / 8) * 10, (blocksize.x / 8) * 20), "Tetris Game", sf::Style::Default);
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 4; j++) {
+			blocks.push_back({ block[i][j].});
+			block[i][j].setTextureRect(sf::IntRect(j * 32, i * 32, blocksize.x / 8, blocksize.y / 5));
+		}
+	}
+	blocks.push_back(block[rand() % 2][rand() % 4]);
+	while (window.isOpen())
+	{
+		deltaTime = clock.restart().asSeconds();
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+				window.close();
+		}
+		for (size_t i = 0; i < blocks.size(); i++) {
+			if (blocks[i].getPosition().y + blocks[i].getGlobalBounds().height < window.getSize().y) {
+				blocks[i].move(0, 50 * deltaTime);
+			}
+		}
+		window.clear();
+		for (size_t i = 0; i < blocks.size(); i++) {
+			window.draw(blocks[i]);
+		}
+		window.display();
+	}
 
-    while (window.isOpen())
-    {
-        sf::Time elapsed = clock.getElapsedTime();
-        sf::Time t1=sf::milliseconds(1000);
-        if (elapsed >= t1 && block[0].getPosition().y + 32 < 640) {
-            block[0].move(sf::Vector2f(0.f, 16.f));
-            clock.restart();
-        }
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-                window.close();
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && block[0].getPosition().y + 32 < 640)
-                block[0].move(sf::Vector2f(0.f,16.f));
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && block[0].getPosition().x > 0 && block[0].getPosition().y + 32 < 640)
-                block[0].move(sf::Vector2f(-16.f, 0.f));
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && block[0].getPosition().x + 32 < 320 && block[0].getPosition().y + 32 < 640)
-                block[0].move(sf::Vector2f(16.f, 0.f));
-        }
-        window.clear();
-        window.draw(block[0]);
-        window.display();
-    }
-
-    return 0;
+	return 0;
 }
